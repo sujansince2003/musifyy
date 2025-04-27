dca4a834926c15c90a5e0080be1df06791c971c9
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-04-28 01:30:54 +0545

    built landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,20 @@
+import CallToAction from "@/components/general/Calltoaction";
+import Features from "@/components/general/Features";
+import Footer from "@/components/general/Footer";
+import Hero from "@/components/general/Hero";
+import HowItWorks from "@/components/general/Howitworks";
+import Showcase from "@/components/general/Showcase";
+
 export default function Home() {
-  console.log(process.env.NEXT_PUBLIC_Client_ID);
-  return <div>hello</div>;
+  return (
+    <div>
+      {" "}
+      <Hero />
+      <Features />
+      <HowItWorks />
+      <Showcase />
+      <CallToAction />
+      <Footer />
+    </div>
+  );
 }
