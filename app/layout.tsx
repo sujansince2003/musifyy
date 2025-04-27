f6eca902fe363977beaf741816764fae2f742d3d
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-04-27 23:35:39 +0545

    added session provider wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/general/Header";
+import SessionProviderWrapper from "./Providers/SessionProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +27,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        {children}
+        <SessionProviderWrapper>
+          <Header />
+          {children}
+        </SessionProviderWrapper>
       </body>
     </html>
   );
