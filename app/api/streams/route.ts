0d8965363c3aea93d1a99a0017ac77a657cc7439
Sujan Khatri <99068384+sujansince2003@users.noreply.github.com>
2025-04-29 17:17:01 +0545

    api routes for creating stream

---
+++ app/api/streams/route.ts
@@ -0,0 +1,72 @@
+import prisma from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod"
+
+
+const YT_URL_REGEX = new RegExp("^https:\/\/www\.youtube\.com\/watch\?v=[\w-]+$")
+
+
+
+
+
+
+
+
+const StreamSchema = z.object({
+    userId: z.string(),
+    url: z.string()
+})
+
+
+export async function POST(req: NextRequest) {
+
+
+    try {
+        const dataFromclient = await req.json();
+        const validData = StreamSchema.safeParse(dataFromclient)
+
+        if (!validData.success) {
+            return NextResponse.json({ msg: "Invalid Data from Client side" })
+        }
+        const data = validData.data;
+
+        const validYTurl = YT_URL_REGEX.test(data.url);
+
+        if (!validYTurl) {
+            return NextResponse.json({ msg: "Invalid Youtube URl" }, { status: 401 })
+        }
+
+
+
+        const extractedId = data.url.split("?v=")[1];
+
+
+        const createStream = await prisma.stream.create({
+            data: {
+                userId: data.userId,
+                url: data.url,
+                extractedId,
+                type: "YouTube",
+                streamName: "stream1"
+
+            }
+        })
+
+        return NextResponse.json({ msg: "Created Stream successfully", createStream }, { status: 200 })
+
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ msg: "Error while creating stream" })
+
+    }
+
+
+
+
+
+}
+
+
+export async function GET() {
+
+}
